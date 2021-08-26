846d77f472980e2b6cddabd6851054fd965d45be
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-26 15:34:37 +0530

    Added:DailyWage

--- src/main/java/org/Blz/Wagedata/WageDetails.java
+++ src/main/java/org/Blz/Wagedata/WageDetails.java
@@ -6,13 +6,15 @@ public class WageDetails
     {
         System.out.println( "Welcome to the Employee Wage Assignment" );
         int IS_FULL_TIME=1;
+        int EMP_WAGE_PER_HOUR=20;
+        int WORK_HOURS=8;
         double empCheck= (Math.floor(Math.random()*10)%2);
-        System.out.println( empCheck);
+        System.out.println("Employee wage is ");
         if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is present");
+            System.out.println(EMP_WAGE_PER_HOUR*WORK_HOURS);
         }
         else  {
-            System.out.println("Employe is absent");
+            System.out.println("0");
         }
 
     }
