1282a7d0dde4b7ad43035c90a6582d4594b5bb10
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-26 15:18:22 +0530

    Added:RandomPresentOrAbsent

--- src/main/java/org/Blz/Wagedata/WageDetails.java
+++ src/main/java/org/Blz/Wagedata/WageDetails.java
@@ -5,5 +5,15 @@ public class WageDetails
     public static void main( String[] args )
     {
         System.out.println( "Welcome to the Employee Wage Assignment" );
+        int IS_FULL_TIME=1;
+        double empCheck= (Math.floor(Math.random()*10)%2);
+        System.out.println( empCheck);
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee is present");
+        }
+        else  {
+            System.out.println("Employe is absent");
+        }
+
     }
 }
