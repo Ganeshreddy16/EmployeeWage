4f23890554dd85088049c7267620cbe90b2e6924
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-26 15:06:13 +0530

    Added: Project structure

---
+++ src/main/java/org/Blz/Wagedata/WageDetails.java
@@ -0,0 +1,9 @@
+package org.Blz.Wagedata;
+
+public class WageDetails
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Employee Wage Assignment" );
+    }
+}
