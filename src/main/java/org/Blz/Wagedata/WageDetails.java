45c28942ffa1701f2e807fe7eab869eed529a502
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-26 17:03:21 +0530

    Added :Switch case

--- src/main/java/org/Blz/Wagedata/WageDetails.java
+++ src/main/java/org/Blz/Wagedata/WageDetails.java
@@ -2,30 +2,34 @@ package org.Blz.Wagedata;
 
 public class WageDetails
 {
+    public static final int IS_FULL_TIME=2;
+    public static final int IS_PART_TIME=1;
+    public static final int EMP_WAGE_PER_HOUR=20;
+
     public static void main( String[] args )
     {
-        int IS_FULL_TIME=2;
-        int IS_PART_TIME=1;
-        int EMP_WAGE_PER_HOUR=20;
         int WorkHours=0;
         double empCheck= (Math.floor(Math.random()*10)%3);
         System.out.println("Employee wage is ");
-        if (empCheck == IS_FULL_TIME)
-        {
-            WorkHours=8;
-            System.out.println(EMP_WAGE_PER_HOUR*WorkHours);
+        switch((int)empCheck) {
+            case IS_FULL_TIME:
+            {
+                WorkHours = 8;
+                System.out.println(EMP_WAGE_PER_HOUR * WorkHours);
+            }
+            break;
+            case IS_PART_TIME:
+            {
+                WorkHours = 4;
+                System.out.println(EMP_WAGE_PER_HOUR * WorkHours);
+            }
+            break;
+            default:
+            {
+                WorkHours = 0;
+                System.out.println(EMP_WAGE_PER_HOUR * WorkHours);
+            }
         }
-        else if (empCheck == IS_PART_TIME)
-        {
-            WorkHours = 4;
-            System.out.println(EMP_WAGE_PER_HOUR * WorkHours);
-        }
-        else
-        {
-                WorkHours=0;
-                System.out.println( EMP_WAGE_PER_HOUR* WorkHours);
-        }
-
 
     }
 }
