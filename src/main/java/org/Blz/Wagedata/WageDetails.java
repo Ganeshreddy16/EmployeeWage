7dc687351f7346de13c8529ef69463e8e105a82f
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-26 17:50:26 +0530

    Added : ForLoopCondition

--- src/main/java/org/Blz/Wagedata/WageDetails.java
+++ src/main/java/org/Blz/Wagedata/WageDetails.java
@@ -6,28 +6,29 @@ public class WageDetails
     public static final int IS_PART_TIME=1;
     public static final int EMP_WAGE_PER_HOUR=20;
     public static final int WORKING_DAYS=20;
-    public static void main( String[] args )
-    {
-        int WorkHours=0;
-        double empCheck= (Math.floor(Math.random()*10)%3);
-        System.out.println("Wage For");
-        switch((int)empCheck) {
-            case IS_FULL_TIME:
-            {
-                WorkHours = 8;
-                System.out.println("Full time Emp is " + EMP_WAGE_PER_HOUR * WorkHours * WORKING_DAYS);
-            }
-            break;
-            case IS_PART_TIME:
-            {
-                WorkHours = 4;
-                System.out.println("Part time Emp is " + EMP_WAGE_PER_HOUR * WorkHours*WORKING_DAYS);
-            }
-            break;
-            default:
-            {
-                WorkHours = 0;
-                System.out.println("Absentee is "+EMP_WAGE_PER_HOUR * WorkHours*WORKING_DAYS);
+
+            public static void main( String[] args )
+    { int WorkHours=0;
+        int Day;
+        for (Day=1; Day < WORKING_DAYS; Day++) {
+            System.out.println("Day "+ Day);
+            double empCheck = (Math.floor(Math.random() * 10) % 3);
+            System.out.println("Wage For");
+            switch ((int) empCheck) {
+                case IS_FULL_TIME: {
+                    WorkHours = 8;
+                    System.out.println("Full time Emp is " + EMP_WAGE_PER_HOUR * WorkHours * WORKING_DAYS);
+                }
+                break;
+                case IS_PART_TIME: {
+                    WorkHours = 4;
+                    System.out.println("Part time Emp is " + EMP_WAGE_PER_HOUR * WorkHours * WORKING_DAYS);
+                }
+                break;
+                default: {
+                    WorkHours = 0;
+                    System.out.println("Absentee is " + EMP_WAGE_PER_HOUR * WorkHours * WORKING_DAYS);
+                }
             }
         }
 
